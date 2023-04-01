2c89669d2a28400d67a61cbdbf738e5e187770f1
miguevelas <bauti.velas.miguel@gmail.com>
2023-04-01 16:54:03 -0300

    primer commit

---
+++ styles.js
@@ -0,0 +1,33 @@
+function solicitarNombre (){
+    let nombreIngresado = prompt ("Ingresar nombre")
+    alert ("Bienvenido " + nombreIngresado)
+}
+solicitarNombre ()
+
+const inputBase = document.querySelector('#baseImponible');
+let inputIVA = document.querySelector('#IVA');
+let inputTotal = document.querySelector('#total');
+const tipoIVA = 0.21;
+function Calcular() {
+    const base = Number(inputBase.value);
+    const IVA = base * tipoIVA;
+    inputIVA.value = IVA; 
+    const total = base + IVA;
+    inputTotal.value = total;
+}
+
+var numero1 = parseInt(prompt ("Ingrese un numero para comparar", 0));
+var numero2 = parseInt(prompt("Ingrese el otro numero", 0));
+
+if (numero1 >=200 && numero2 <= 1500){
+    alert("Los numeros ingresados estan entre 200 y 1500 y son: "+numero1+" y "+numero2);
+} else {
+    alert("Los numeros ingresados NO estan entre 200 y 1500 y son: "+numero1+" y "+numero2);
+}
+
+var numero1 = parseInt(prompt("Ingrese un numero para repetir el mensaje",0));
+for (let i=3; i <=numero1; i++){
+    alert("Hola");
+}
+
+
