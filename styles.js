378cec4e25eeaf47826ab16f0e4dec7bcc0bc16e
miguevelas <bauti.velas.miguel@gmail.com>
2023-04-15 22:31:47 -0300

    correcciones segunda pre entrega

--- styles.js
+++ styles.js
@@ -15,10 +15,8 @@ function Calcular() {
     const total = base + IVA;
     inputTotal.value = total;
 }
-
 var numero1 = parseInt(prompt ("Ingrese un numero para comparar", 0));
 var numero2 = parseInt(prompt("Ingrese el otro numero", 0));
-
 if (numero1 >=200 && numero2 <= 1500){
     alert("Los numeros ingresados estan entre 200 y 1500 y son: "+numero1+" y "+numero2);
 } else {
@@ -30,4 +28,26 @@ for (let i=3; i <=numero1; i++){
     alert("Hola");
 }
 
+const listaProductos = ["lenteja", "garbanzo", "poroto"]
+console.log(listaProductos.length);
+
+listaProductos.push('papa')
+console.log(listaProductos.length)
+console.log(listaProductos)
+
+class Almacen{
+    constructor(legumbre, kilos, color){
+        this.legumbre = legumbre;
+        this.kilos = kilos;
+        this.color = color;
+    }
+    leer(){
+        console.log ("Este producto es " + (this.legumbre) + " y pesa kg " + (this.kilos));
+    }
+}
+
+let almacen1 = new Almacen("lenteja", 2, "verde");
+almacen1.leer();
+let almacen2 = new Almacen("garbanzo", 5, "amarillo");
+almacen2.leer();
 
